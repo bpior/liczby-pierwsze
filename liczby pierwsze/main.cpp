8a796c5027d425bf03494b2c27ecaf074db045e9
bpior <bartlomiejpiorczynski@gmail.com>
2021-12-22 21:51:39 +0100

    Update main.cpp
    szukanie liczb pierwszych

--- liczby pierwsze/main.cpp
+++ liczby pierwsze/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
+#include <math.h> //sqrt function used from this library
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(){
+// Variable Declaration
+    int n;
+
+// Get Input Value
+    cout<<"Enter the Number : ";
+    cin>>n;
+
+    cout <<"List Of Prime Numbers Below "<<n<<endl;
+
+//for Loop Block For Find Prime Number
+
+    for (int i=2; i<n; i++)
+        for (int j=2; j*j<=i; j++)
+        {
+            if (i % j == 0)
+                break;
+            else if (j+1 > sqrt(i)) {
+                cout << i << endl;
+            }
+        }
     return 0;
 }
